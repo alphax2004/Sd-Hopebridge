d7ffdaac5b4154a2270b4c5c06c22652787cd126
alphax2004 <sanjida05alfa@gmail.com>
2026-09-03 09:09:51 +0600

    chore:mongodb connect

---
+++ sdweb/backend/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(`Error connecting to database ${err}`);
+    process.exit(1);
+  }
+};
+
+connectDB();
